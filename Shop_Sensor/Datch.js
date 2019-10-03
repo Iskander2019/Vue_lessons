b0455c9b3c830f906b4a1f3b0fef0af4482dd453
Iskander2019 <anda3036@gmail.com>
2019-10-03 15:11:18 +0300

    Работа 2  от 3.10.19

---
+++ Shop_Sensor/Datch.js
@@ -0,0 +1,13 @@
+const Datchiki = [
+{name:"DGG-14", GAS:"Гор газ", interface:"RS-485", P:"0.8W", image:"../../Images/DGG.jpg"}
+]
+new Vue({
+    el : "#Contner",
+    data : {
+        datch:Datchiki
+
+
+    }
+    
+    
+})
