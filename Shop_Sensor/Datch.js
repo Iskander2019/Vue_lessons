15a0685f532bdc9e5a256cc3efc6d73b48437475
Iskander2019 <anda3036@gmail.com>
2019-10-04 08:19:28 +0300

    Работа 1 от 4.10.19

--- Shop_Sensor/Datch.js
+++ Shop_Sensor/Datch.js
@@ -1,13 +1,28 @@
-const Datchiki = [
-{name:"DGG-14", GAS:"Гор газ", interface:"RS-485", P:"0.8W", image:"../../Images/DGG.jpg"}
+function dat(name, type, interface, P, image) {
+ //   return {name:name, type:type, interface, P, image
+    return {name:name, type:type, interrface:interface, P:P, image:image
+    }
+}
+
+const datchiki = [
+dat("DGG-14", "Гор газ", "RS-485", "0.8W", "../../Images/DGG.jpg"),
+dat("DUG-14", "Гор газ", "RS-485", "0.8W", "../../Images/DUG.jpg"),
+dat("DP-14", "Гор газ", "RS-485", "0.8W", "../../Images/DP.jpg")
 ]
+
 new Vue({
     el : "#Contner",
     data : {
-        datch:Datchiki
-
-
-    }
-    
-    
+        datch: datchiki,
+        out2 : "6666666666"
+    }    
 })
+
+new Vue({
+    el:"#ttt",
+    data : {
+        out1:"444444jkgh44"
+    }
+
+})
+
---
+++ Shop_Sensor/Datch1.js
@@ -0,0 +1,9 @@
+const Datchiki = [
+{name:"DGG-14", type:"Гор газ", interface:"RS-485", P:"0.8W", image:"../../Images/DA-14.jpg"}
+]
+new Vue({
+    el : "#Contner",
+    data : {
+        datch:Datchiki
+    }    
+})
