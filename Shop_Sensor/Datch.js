5e799019094595bb2f35fdacacf2af85fb2a019f
Iskander2019 <anda3036@gmail.com>
2019-10-04 10:33:14 +0300

    Работа 2 от 4.10.19

--- Shop_Sensor/Datch.js
+++ Shop_Sensor/Datch.js
@@ -1,20 +1,29 @@
 function dat(name, type, interface, P, image) {
- //   return {name:name, type:type, interface, P, image
-    return {name:name, type:type, interrface:interface, P:P, image:image
-    }
+
+   return {name:name, type:type, interface:interface, P:P, image:image
+   }
 }
 
+
+
 const datchiki = [
-dat("DGG-14", "Гор газ", "RS-485", "0.8W", "../../Images/DGG.jpg"),
-dat("DUG-14", "Гор газ", "RS-485", "0.8W", "../../Images/DUG.jpg"),
-dat("DP-14", "Гор газ", "RS-485", "0.8W", "../../Images/DP.jpg")
+dat("DGG-14", "Гор газ", "RS-485", "0.8W", "../../Images/DA-14.jpg"),
+dat("DUG-14", "Угарный газ", "RS-485", "1,2W", "../../Images/DUG-14.jpg"),
+dat("DP-14", "Пропан", "RS-485", "1,6W", "../../Images/DM-14.jpg")
 ]
 
 new Vue({
     el : "#Contner",
     data : {
         datch: datchiki,
-        out2 : "6666666666"
+        out2 : "0000000000",
+        dat1 : datchiki[0]
+    },
+    methods: {
+        selectDat: function(index){
+            console.log("CLICK", index);
+            this.dat1=datchiki[index]
+        }
     }    
 })
 
